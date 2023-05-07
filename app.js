6b5a9b03326348bd4ab2258a65912eefa7d8bfe1
noobwale <wallacesheundah@gmail.com>
2023-05-07 04:20:13 +0300

    updated app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const board = document.querySelector(".board");
+board.style.gridTemplateColumns = "repeat( 16 , 1fr )";
+board.style.gridTemplateRows = "repeat( 16 , 1fr )";
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "yellow";
+    square.style.border = "1px solid red";
+    board.insertAdjacentElement("beforeend", square);    
+}
