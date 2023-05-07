37c531a01f4e72de2500f8d7bd7810ffa286a49f
noobwale <wallacesheundah@gmail.com>
2023-05-07 05:23:58 +0300

    updated styles.css

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat( 16 , 1fr )";
-board.style.gridTemplateRows = "repeat( 16 , 1fr )";
 
-for (let i = 0; i < 256; i++) {
+// This function populates the board with the number of size input by the user
+function populateGrid(size) {
+  let board = document.querySelector(".board");
+  let grids = board.querySelectorAll("div");
+  grids.forEach((div) => remove(div));
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr )`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr )`;
+
+  let gSize = size * size;
+
+  for (let i = 0; i < gSize; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = "yellow";
-    square.style.border = "1px solid red";
-    board.insertAdjacentElement("beforeend", square);    
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    })
+    board.insertAdjacentElement("beforeend", square);
+  }
 }
+
+function changeGridSize (input) {
+    if (input >= 2 && input <= 100) {
+        populateGrid(input);
+    };
+};
